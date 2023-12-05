a3ce846bf76cc05ccaa2e2494b4bb0b88679fe1a
Yaseen <ahm3d.yaseen@gmail.com>
2023-12-05 06:35:26 +0500

    contact index

---
+++ contact-management-system/resources/views/contacts.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Contacts') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table>
+                        @foreach ($contacts->all() as $contact) {
+                            <tr>
+                                <td>$contact->id</td>
+                                <td>$contact->name</td>
+                                <td>$contact->phone</td>
+                                <td>$contact->email</td>
+                                <td>$contact->address</td>
+                            </tr>
+                        }
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
